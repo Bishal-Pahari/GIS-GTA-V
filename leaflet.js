1d8a349ab3845f384236c804cd737e43f0f4c375
Bishal Pahari <bishal.pahari53@gmail.com>
2023-11-29 22:29:03 +0545

    feat: remove zoom button

--- leaflet.js
+++ leaflet.js
@@ -3,7 +3,7 @@ const center_y = 172.8;
 const scale_x = 0.02072;
 const scale_y = 0.0205;
 
-CUSTOM_CRS = L.extend({}, L.CRS.Simple, {
+const CUSTOM_CRS = L.extend({}, L.CRS.Simple, {
   projection: L.Projection.LonLat,
   scale: function (zoom) {
     return Math.pow(2, zoom);
@@ -28,22 +28,6 @@ var SateliteStyle = L.tileLayer("mapStyles/styleSatelite/{z}/{x}/{y}.jpg", {
   attribution: "Self-driving-car-Project",
   id: "SateliteStyle map",
 });
-var AtlasStyle = L.tileLayer("mapStyles/styleAtlas/{z}/{x}/{y}.jpg", {
-  minZoom: 0,
-  maxZoom: 5,
-  noWrap: true,
-  continuousWorld: false,
-  attribution: "Self-driving-car-Project",
-  id: "styleAtlas map",
-});
-var GridStyle = L.tileLayer("mapStyles/styleGrid/{z}/{x}/{y}.png", {
-  minZoom: 0,
-  maxZoom: 5,
-  noWrap: true,
-  continuousWorld: false,
-  attribution: "Self-driving-car-Project",
-  id: "styleGrid map",
-});
 
 var ExampleGroup = L.layerGroup();
 
@@ -62,14 +46,13 @@ var mymap = L.map("map", {
   center: [0, 0],
   zoom: 4.5,
   scrollWheelZoom: false,
+  zoomControl: false, // Remove zoom control
   attributionControl: false,
 });
 
 var layersControl = L.control
   .layers({
     Satelite: SateliteStyle,
-    Atlas: AtlasStyle,
-    Grid: GridStyle,
   })
   .addTo(mymap);
 
@@ -92,7 +75,7 @@ mymap.on("click", function (e) {
   ExampleGroup.clearLayers();
 
   // Add a new marker
-  var marker = L.marker([lat, lng], { icon: customIcon() })
+  marker = L.marker([lat, lng], { icon: customIcon() })
     .addTo(ExampleGroup)
     .bindPopup("<b>X: " + lng.toFixed(3) + " | Y: " + lat.toFixed(3) + "</b>");
 });
@@ -108,16 +91,15 @@ $("#formular").submit(function (event) {
   var xpoz = $("#xinput").val();
   var ypoz = $("#yinput").val();
 
-  // Clear existing markers in ExampleGroup
   ExampleGroup.clearLayers();
 
   // Add a new marker
-  var azurirajmarker = L.marker([ypoz, xpoz], { icon: customIcon() })
+  marker = L.marker([ypoz, xpoz], { icon: customIcon() })
     .addTo(ExampleGroup)
     .bindPopup("<b>X: " + xpoz + " | Y: " + ypoz + "</b>");
 
   mymap.setView([ypoz, xpoz], mymap.getZoom());
   $("#markerSkripta")
     .empty()
-    .append("<script>" + azurirajmarker + "</script>");
+    .append("<script>" + marker + "</script>");
 });
