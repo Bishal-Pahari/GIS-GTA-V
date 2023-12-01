d9efff3acaa272e460a13a6ad0057147e3cb7577
Bishal Pahari <bishal.pahari53@gmail.com>
2023-12-01 22:59:08 +0545

    feat: Add search bar and search bar suggestion

---
+++ autoSuggestion.js
@@ -0,0 +1,463 @@
+let locationsData = {
+  locations: [
+    {
+      name: "Maze Bank Arena",
+      coordinates: { X: -291.988, Y: -1979.268 },
+    },
+    {
+      name: "Los Santos International Airport",
+      coordinates: { X: -1034.6, Y: -2733.6 },
+    },
+    {
+      name: "Vinewood Sign",
+      coordinates: { X: 640.1, Y: 1688.6 },
+    },
+    {
+      name: "Mount Chiliad",
+      coordinates: { X: 425.1, Y: 5616.2 },
+    },
+    {
+      name: "Pacific Standard Public Deposit Bank",
+      coordinates: { X: 231.8, Y: 216.0 },
+    },
+    {
+      name: "Fort Zancudo",
+      coordinates: { X: -2047.4, Y: 3132.1 },
+    },
+    {
+      name: "Del Perro Pier",
+      coordinates: { X: -1850.5, Y: -122.3 },
+    },
+    {
+      name: "Sandy Shores Airfield",
+      coordinates: { X: 1729.9, Y: 3295.2 },
+    },
+    {
+      name: "Chumash",
+      coordinates: { X: -3192.6, Y: 1100.0 },
+    },
+    {
+      name: "Los Santos City Hall",
+      coordinates: { X: 285.4, Y: -602.7 },
+    },
+    {
+      name: "Grapeseed",
+      coordinates: { X: 1687.5, Y: 4775.7 },
+    },
+    {
+      name: "Los Santos Customs (Burton)",
+      coordinates: { X: -365.5, Y: -131.3 },
+    },
+    {
+      name: "Humane Labs and Research",
+      coordinates: { X: 3530.5, Y: 3663.2 },
+    },
+    {
+      name: "Eclipse Towers (Apartment)",
+      coordinates: { X: -777.2, Y: 319.6 },
+    },
+    {
+      name: "Chinatown",
+      coordinates: { X: -1478.1, Y: -881.4 },
+    },
+    {
+      name: "Vespucci Beach",
+      coordinates: { X: -1406.1, Y: -974.2 },
+    },
+    {
+      name: "Galileo Observatory",
+      coordinates: { X: -438.8, Y: 1076.7 },
+    },
+    {
+      name: "The Diamond Casino & Resort",
+      coordinates: { X: 930.1, Y: 48.7 },
+    },
+    {
+      name: "Grove Street",
+      coordinates: { X: 83.1, Y: -1956.0 },
+    },
+    {
+      name: "Maze Bank Tower",
+      coordinates: { X: -75.2, Y: -827.0 },
+    },
+    {
+      name: "Griffith Observatory",
+      coordinates: { X: -438.8, Y: 1076.7 },
+    },
+    {
+      name: "Davis Quartz Quarry",
+      coordinates: { X: 2948.6, Y: 2773.4 },
+    },
+    {
+      name: "Los Santos Golf Club",
+      coordinates: { X: -1221.7, Y: 70.4 },
+    },
+    {
+      name: "Vespucci Canals",
+      coordinates: { X: -1003.0, Y: -14.7 },
+    },
+    {
+      name: "FIB Building",
+      coordinates: { X: 150.4, Y: -754.2 },
+    },
+    {
+      name: "Palmer-Taylor Power Station",
+      coordinates: { X: 731.9, Y: 4004.3 },
+    },
+    {
+      name: "Mirror Park",
+      coordinates: { X: 126.5, Y: -1447.1 },
+    },
+    {
+      name: "Lago Zancudo",
+      coordinates: { X: -2362.4, Y: 2786.2 },
+    },
+    {
+      name: "Merryweather Dock",
+      coordinates: { X: 489.8, Y: -3333.2 },
+    },
+    {
+      name: "Hawick",
+      coordinates: { X: 356.3, Y: -1386.9 },
+    },
+    {
+      name: "La Puerta",
+      coordinates: { X: -1145.9, Y: -1463.7 },
+    },
+    {
+      name: "Downtown Cab Co.",
+      coordinates: { X: 911.4, Y: -150.8 },
+    },
+    {
+      name: "Los Santos Storm Drain",
+      coordinates: { X: 401.1, Y: -1635.5 },
+    },
+    {
+      name: "Richman Glen",
+      coordinates: { X: -1467.8, Y: 4432.5 },
+    },
+    {
+      name: "Maze Bank Foreclosures",
+      coordinates: { X: -1471.1, Y: -520.0 },
+    },
+    {
+      name: "Mirror Park Lake",
+      coordinates: { X: 1236.8, Y: -437.3 },
+    },
+    {
+      name: "Vespucci Helipad",
+      coordinates: { X: -753.3, Y: -1714.5 },
+    },
+    {
+      name: "Tongva Hills",
+      coordinates: { X: -689.1, Y: 4585.3 },
+    },
+    {
+      name: "Los Santos Observatory",
+      coordinates: { X: 3886.5, Y: -1035.7 },
+    },
+    {
+      name: "Pillbox Hill",
+      coordinates: { X: 66.9, Y: 705.4 },
+    },
+    {
+      name: "Vinewood Bowl",
+      coordinates: { X: 647.0, Y: 268.4 },
+    },
+    {
+      name: "GWC and Golfing Society",
+      coordinates: { X: -1033.9, Y: 6516.2 },
+    },
+    {
+      name: "Tataviam Mountains",
+      coordinates: { X: 1747.0, Y: 4764.6 },
+    },
+    {
+      name: "Galileo Park",
+      coordinates: { X: -590.9, Y: 354.5 },
+    },
+    {
+      name: "Lester's House",
+      coordinates: { X: 1274.3, Y: -1718.5 },
+    },
+    {
+      name: "Grand Senora Desert",
+      coordinates: { X: 2641.9, Y: 3792.7 },
+    },
+    {
+      name: "Zancudo River",
+      coordinates: { X: 86.8, Y: 328.7 },
+    },
+    {
+      name: "Mirror Park Dog Park",
+      coordinates: { X: 1136.4, Y: -1610.7 },
+    },
+    {
+      name: "La Mesa",
+      coordinates: { X: 945.8, Y: -2400.4 },
+    },
+    {
+      name: "Land Act Dam",
+      coordinates: { X: 1666.5, Y: 4837.7 },
+    },
+    {
+      name: "Los Santos Customs (LSIA)",
+      coordinates: { X: -1143.4, Y: -1985.7 },
+    },
+    {
+      name: "Richman Mansion",
+      coordinates: { X: -1223.1, Y: 340.7 },
+    },
+    {
+      name: "San Chianski Mountain Range",
+      coordinates: { X: 590.5, Y: 5593.8 },
+    },
+    {
+      name: "Union Depository",
+      coordinates: { X: 3.7, Y: -746.6 },
+    },
+    {
+      name: "Zancudo Bridge",
+      coordinates: { X: -1287.3, Y: 368.5 },
+    },
+    {
+      name: "LS River",
+      coordinates: { X: 661.1, Y: -2489.7 },
+    },
+    {
+      name: "Ludendorff",
+      coordinates: { X: 618.1, Y: 200.0 },
+    },
+    {
+      name: "Paparazzo",
+      coordinates: { X: -532.0, Y: -318.8 },
+    },
+    {
+      name: "Little Seoul",
+      coordinates: { X: -892.3, Y: -224.0 },
+    },
+    {
+      name: "O'Neil Ranch",
+      coordinates: { X: 2441.2, Y: 4968.9 },
+    },
+    {
+      name: "El Burro Heights",
+      coordinates: { X: 1391.3, Y: -2051.5 },
+    },
+    {
+      name: "Redwood Lights Track",
+      coordinates: { X: 842.4, Y: -2327.3 },
+    },
+    {
+      name: "Arcadius Business Center",
+      coordinates: { X: -115.5, Y: -606.3 },
+    },
+    {
+      name: "Los Santos Country Club",
+      coordinates: { X: 72.9, Y: -1095.5 },
+    },
+    {
+      name: "Altruist Cult Camp",
+      coordinates: { X: -1172.9, Y: 4925.6 },
+    },
+    {
+      name: "Sandy Shores Medical Center",
+      coordinates: { X: 1839.3, Y: 3672.5 },
+    },
+    {
+      name: "NOOSE Headquarters",
+      coordinates: { X: 2535.3, Y: -384.5 },
+    },
+    {
+      name: "Lago Zancudo Swamp",
+      coordinates: { X: -2232.5, Y: 303.2 },
+    },
+    {
+      name: "Fort Zancudo Runway",
+      coordinates: { X: -2373.0, Y: 3157.0 },
+    },
+    {
+      name: "Grapeseed Cow Farm",
+      coordinates: { X: 2445.4, Y: 4976.8 },
+    },
+    {
+      name: "Sandy Shores Trailer Park",
+      coordinates: { X: 1030.5, Y: 4811.5 },
+    },
+    {
+      name: "Maze Bank Foreclosures (West Vinewood)",
+      coordinates: { X: -203.6, Y: -1013.9 },
+    },
+    {
+      name: "Rockford Hills City Hall",
+      coordinates: { X: -558.3, Y: -1455.3 },
+    },
+    {
+      name: "Zancudo Oil Refinery",
+      coordinates: { X: -1382.3, Y: 6317.5 },
+    },
+    {
+      name: "Paleto Bay Sheriff's Office",
+      coordinates: { X: -439.4, Y: 6011.0 },
+    },
+    {
+      name: "Banham Canyon",
+      coordinates: { X: -3086.5, Y: 339.2 },
+    },
+    {
+      name: "Los Santos Golf Club (Richman)",
+      coordinates: { X: -1193.4, Y: 58.3 },
+    },
+    {
+      name: "Cassidy Creek",
+      coordinates: { X: 542.7, Y: 4324.1 },
+    },
+    {
+      name: "Grand Senora Oilfields",
+      coordinates: { X: 1082.3, Y: 2511.6 },
+    },
+    {
+      name: "Raton Canyon",
+      coordinates: { X: -1364.4, Y: 161.2 },
+    },
+    {
+      name: "Port of Los Santos",
+      coordinates: { X: 2475.7, Y: 3226.2 },
+    },
+    {
+      name: "Los Santos Naval Port",
+      coordinates: { X: 838.4, Y: -2802.1 },
+    },
+    {
+      name: "Sandy Shores Motel",
+      coordinates: { X: 1127.5, Y: 3859.6 },
+    },
+    {
+      name: "Kortz Center",
+      coordinates: { X: -2275.0, Y: 380.8 },
+    },
+    {
+      name: "Grapeseed Main Street",
+      coordinates: { X: 1685.5, Y: 4756.9 },
+    },
+    {
+      name: "Los Santos Storm Drain (Canal)",
+      coordinates: { X: 362.9, Y: -1945.5 },
+    },
+    {
+      name: "Tongva Valley",
+      coordinates: { X: -619.0, Y: 2948.0 },
+    },
+    {
+      name: "Los Santos Golf Club (GWC)",
+      coordinates: { X: -957.8, Y: 50.2 },
+    },
+    {
+      name: "Hill Valley Church",
+      coordinates: { X: -246.2, Y: -2128.9 },
+    },
+    {
+      name: "Vespucci Movie Masks",
+      coordinates: { X: -1047.3, Y: -1941.5 },
+    },
+    {
+      name: "Palmer-Taylor Power Station ",
+      coordinates: { X: 2487.4, Y: 2289.3 },
+    },
+    {
+      name: "Great Chaparral",
+      coordinates: { X: -247.3, Y: 6701.3 },
+    },
+    {
+      name: "Cypress Flats",
+      coordinates: { X: 842.5, Y: -274 },
+    },
+  ],
+};
+
+let suggestions = locationsData.locations.map((location) => location.name);
+
+const searchInput = document.querySelector(".searchInput");
+const input = searchInput.querySelector("input");
+const resultBox = searchInput.querySelector(".resultBox");
+const icon = searchInput.querySelector(".icon");
+let linkTag = searchInput.querySelector("a");
+let webLink;
+
+input.onkeyup = (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if (userData) {
+    emptyArray = suggestions.filter((data) => {
+      return data.toLowerCase().startsWith(userData.toLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      return "<li>" + data + "</li>";
+    });
+    searchInput.classList.add("active");
+    showSuggestions(emptyArray);
+    let allList = resultBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++) {
+      allList[i].setAttribute("onclick", "select(this)");
+    }
+  } else {
+    searchInput.classList.remove("active");
+  }
+};
+
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = input.value;
+    listData = "<li>" + userValue + "</li>";
+  } else {
+    listData = list.join("");
+  }
+  resultBox.innerHTML = listData;
+}
+
+document.addEventListener("click", function (e) {
+  const isClickInsideSearchInput = searchInput.contains(e.target);
+  if (!isClickInsideSearchInput) {
+    // Clicked outside the search input, clear the results
+    clearResults();
+  }
+});
+
+input.oninput = (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if (userData) {
+    emptyArray = suggestions.filter((data) => {
+      return data.toLowerCase().startsWith(userData.toLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      return "<li>" + data + "</li>";
+    });
+    searchInput.classList.add("active");
+    showSuggestions(emptyArray);
+    let allList = resultBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++) {
+      allList[i].setAttribute("onclick", "select(this)");
+    }
+  } else {
+    // Input is empty, clear the results
+    clearResults();
+  }
+};
+
+function clearResults() {
+  searchInput.classList.remove("active");
+  resultBox.innerHTML = "";
+}
+
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = input.value;
+    listData = "<li>" + userValue + "</li>";
+  } else {
+    listData = list.join("");
+  }
+  resultBox.innerHTML = listData;
+}
